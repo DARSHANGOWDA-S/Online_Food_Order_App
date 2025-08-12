0204d319b332cfa9a4db5293e41e591d214e639b
DARSHANGOWDA-S <172057394+DARSHANGOWDA-S@users.noreply.github.com>
2025-08-12 18:09:02 +0530

    updated

--- src/main/java/com/orderapp/Online_Order_App/controller/OrderController.java
+++ src/main/java/com/orderapp/Online_Order_App/controller/OrderController.java
@@ -3,15 +3,21 @@ package com.orderapp.Online_Order_App.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.orderapp.Online_Order_App.dto.BillResponse;
 import com.orderapp.Online_Order_App.dto.OrderRequest;
 import com.orderapp.Online_Order_App.dto.PaymentDto;
 import com.orderapp.Online_Order_App.dto.ResponseStructure;
+import com.orderapp.Online_Order_App.entity.Order;
+import com.orderapp.Online_Order_App.entity.OrderStatus;
 import com.orderapp.Online_Order_App.service.OrderService;
 
 @RestController
@@ -42,4 +48,30 @@ public class OrderController {
 		
 		return new ResponseEntity<>(apiResponse,HttpStatus.CREATED);
 	}
+	@DeleteMapping("/{id}/delete")
+	public ResponseEntity<Object> deleteOrder(@PathVariable Integer id) {
+		orderService.deleteOrder(id);
+		
+		return new ResponseEntity<Object>(HttpStatus.NO_CONTENT);
+	}
+	@PatchMapping("/{id}/updateStatus")
+	public ResponseEntity<ResponseStructure<Order>> updateStatus(@PathVariable Integer id,@RequestParam OrderStatus status){
+		Order order = orderService.updateStatusByAdmin(status, id);
+		ResponseStructure<Order> apiResponse = new ResponseStructure<>();
+		apiResponse.setData(order);
+		apiResponse.setMessage("status updated");
+		apiResponse.setStatusCode(HttpStatus.OK.value());
+		return new ResponseEntity<>(apiResponse,HttpStatus.OK);
+		
+	}
+	@PatchMapping("/{id}/cancelOrder")
+	public ResponseEntity<ResponseStructure<String>> cancelOrder(@PathVariable Integer id){
+		String response = orderService.cancelOrder(id);
+		ResponseStructure<String> apiResponse = new ResponseStructure<>();
+		apiResponse.setData(response);
+		apiResponse.setMessage("Order cancelled");
+		apiResponse.setStatusCode(HttpStatus.OK.value());
+		return  ResponseEntity.ok(apiResponse);
+	}
+	
 }
